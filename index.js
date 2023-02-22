7fbdee8b918c22ee6ff79ad0ef9298f31e753424
Aleksander <asava@sibmail.com>
2023-02-23 00:01:28 +0700

    Асинхронное удаление элементов

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const chalk = require('chalk')
-const {addNote, getNotes} = require('./notes.controller')
+const path = require('path')
+const {addNote, getNotes, removeNote} = require('./notes.controller')
 
 const port = 3000
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', 'pages')
+
+app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.use(express.urlencoded({
     extended: true
@@ -15,7 +18,8 @@ app.use(express.urlencoded({
 app.get('/', async (req, res) => {
     res.render('index', {
         title: 'Express App',
-        notes: await getNotes()
+        notes: await getNotes(),
+        created: false
     })
 })
 
@@ -23,7 +27,18 @@ app.post('/', async (req, res) => {
     await addNote(req.body.title)
     res.render('index', {
         title: 'Express App',
-        notes: await getNotes()
+        notes: await getNotes(),
+        created: true
+    })
+})
+
+app.delete('/:id', async (req, res) => {
+    // console.log('id', req.params.id)
+    removeNote(req.params.id)
+    res.render('index', {
+        title: 'Express App',
+        notes: await getNotes(),
+        created: false
     })
 })
 
