ac02a0f6224e3b591ce3994159cb133dc9beaaf6
Aleksander <asava@sibmail.com>
2023-02-22 17:25:35 +0700

    Обработка POST-запроса

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http')
 const chalk = require('chalk')
 const fs = require('fs/promises')
 const path = require('path')
+const {addNote} = require('./notes.controller')
 
 const port = 3000
 
@@ -15,6 +16,26 @@ const server = http.createServer(async (req, res) => {
         'Content-Type': 'text/html'
     })
     res.end(content)
+   } else if (req.method === 'POST') {
+    const body = []
+    res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=utf-8' // Заголовки прописываем для старых браузеров
+    })
+
+    req.on('data', data => {     // on - добавляем событие
+        // console.log('data', data) // data <Buffer 74 69 74 6c 65 3d 68 68 68 68>
+        body.push(Buffer.from(data)) // Buffer.from - трансформация данных
+    })
+
+    req.on('end', () => {                    // 'end'- понимаем когда процес данных завершился
+        // console.log('end', body.toString().split('=')[1].replaceAll('+', ' '))
+        const title =  body.toString().split('=')[1].replaceAll('+', ' ')
+        addNote(title)
+
+        res.end(`Title = ${title}`) // закончили как получили данные
+    })
+
+    
    }
 })
 
@@ -22,18 +43,4 @@ server.listen(port, () => {
     console.log(chalk.green(`Server has been on port ${port}...`))
 })
 
-// const http = require('http')
-// const chalk = require('chalk')
 
-// const port = 3000
-
-// const server = http.createServer((req, res) => {
-//     console.log('Request method', req.method)
-//     console.log('Request url', res.url)
-
-//     res.end('Hello from server!!!')
-// })
-
-// server.listen(port, () => {
-//     console.log(chalk.green(`Server has been on port ${port}...`))
-// })
