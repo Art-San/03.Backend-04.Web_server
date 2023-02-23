f1b143840da6a93d13346a16b9d9d05769dd8aed
Aleksander <asava@sibmail.com>
2023-02-23 11:20:54 +0700

    списал

--- notes.controller.js
+++ notes.controller.js
@@ -44,48 +44,19 @@ async function removeNote(id) {
   console.log(chalk.red(`Note with id="${id}" has been removed.`))
 }
 
+async function updateNote(noteData) {
+  const notes = await getNotes()
+  const index = notes.findIndex(note => note.id === noteData.id)
+  if (index >= 0) {
+    notes[index] = { ...notes[index], ...noteData }
+    await saveNotes(notes)
+    console.log(chalk.bgGreen(`Note with id="${noteData.id}" has been updated!`))
+  }
+}
+
 module.exports = {
-  addNote, getNotes, removeNote
+  addNote, getNotes, removeNote, updateNote
 }
 
 
 
-// const fs = require('fs/promises')
-// const path = require('path')
-// const chalk = require('chalk')
-
-// const notesPath = path.join(__dirname, 'db.json')
-
-// async function addNote(title) {
-//     const notes = await getNotes()
-
-//     const note = {
-//         title,
-//         id: Date.now().toString()
-//     }
-
-//     notes.push(note)
-
-//     await fs.writeFile(notesPath, JSON.stringify(notes))
-//     console.log(chalk.bgWhite('Note was added!'))
-// }
-
-
-// async function getNotes() {
-//     const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
-//     return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
-// }
-
-// async function printNotes() {
-//     const notes = await getNotes()
-
-//     console.log(chalk.bgBlue('Here is the list of notes'))
-//     notes.forEach(note => {
-//         console.log(chalk.blue(note.title))
-//     })
-// }
-
-
-// module.exports = {
-//     addNote, printNotes
-// }
